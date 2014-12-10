42dca1dcf92cc9e381471fb80fdf0d82a971e7cf
jteal <jacob.a.teal@gmail.com>
2014-12-10 19:22:15 -0300

    added basic structure

---
+++ structure.js
@@ -0,0 +1,18 @@
+var Day = function(date, orders){
+    this.date = date;
+    this.orders = orders;
+};
+
+var Order = function(name, pedi){
+    this.name = name;
+    this.pedi = pedi;
+}
+
+var empanadas = (function(){
+
+    
+
+
+})();
+
+empanadas.init()
