6150c9078bc2195be3739c9141e58fe6c0ce9729
dragoon013 <jacob.a.teal@gmail.com>
2014-12-10 20:33:17 -0300

    got the general outline of the program done. Need specifics now

--- structure.js
+++ structure.js
@@ -1,16 +1,40 @@
-var Day = function(date, orders){
-    this.date = date;
-    this.orders = orders;
-};
-
-var Order = function(name, pedi){
-    this.name = name;
-    this.pedi = pedi;
-}
+//empanadas, yo
 
 var empanadas = (function(){
 
+    //the array to be filled with individual orders
+    var orders = [];    
+    var orderlines = [];
+    var sumUser = 0;
+    var tip = 0;
     
+    return{
+
+	prices: {};
+
+	init: function(){
+	    
+	    getFromDB(date);
+	    sumOrders();
+	},
+
+	getFromDB: function(date){
+	    db = DB(date);
+
+	},
+	
+	sumOrders: function(){
+	    
+	    for(var i = 0; i < db.orders.length; i++){
+		for(var j = 0; j < db.orders[i].orderlines.length; j++){
+		    sumUser += orderlines[j].amount * prices[orderlines[j].abbreviation];
+		}	
+	    }
+	
+	    sumUser += sumeUser*.1;
+	}
+
+    }
 
 
 })();
