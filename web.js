07f853b952cbbf8cbb922eb0bc1bee9ab0140229
dragoon013 <jacob.a.teal@gmail.com>
2014-12-10 21:09:56 -0300

    fixed up

--- web.js
+++ web.js
@@ -18,49 +18,41 @@ addon.webhook('room_message', /^\/hello$/, function *() {
 app.listen();
 
 
-var empanadas = (function(){
 
-    //the array to be filled with individual orders
-    var orders = [];    
-    var abrevs = [];
-    var sumUser = 0;
-    var tip = 0;
+var abrevs = [];
+var sumUser = 0;
+var tip = 0;
 
 
-    return{
-	
-	userRet:{};
-	prices: {};
 
-	init: function(){
+
+userRet:{};
+
+init: function(){
+    
+    getFromDB(date);
+    sumOrders();
+},
+
+getFromDB: function(date){
+    db = getOrder(date);
+    
+},
+
+sumOrders: function(){
 	    
-	    getFromDB(date);
-	    sumOrders();
-	},
-
-	getFromDB: function(date){
-	    db = getOrder(date);
-
-	},
-	
-	sumOrders: function(){
+    for(var i = 0; i < db.orders.length; i++){
+	for(var j = 0; j < db.orders[i].orderlines.length; j++){
+	    sumUser += db.orders[i].orderlines[j].amount * empanadas.Menu[db.orders[i].orderlines[j].abbreviation];
+	    abrevs.push() = db.orders[i].orderlines[j].abbreviation;
 	    
-	    for(var i = 0; i < db.orders.length; i++){
-		for(var j = 0; j < db.orders[i].orderlines.length; j++){
-		    sumUser += orderlines[j].amount * empanadas.prices[orderlines[j].abbreviation];
-		    abrevs.push() = orderlines[j].abbreviation;
-		    
-		}	
-	    }
-	    
-	    sumUser += sumeUser*.1;
-	    empanadas.userRet["abbreviations"] = abrevs;
-	    empanadas.userRet["sum"] = sumUser;
-	}
-
+	}	
     }
+	    
+    sumUser += sumeUser*.1;
+    empanadas.userRet["abbreviations"] = abrevs;
+    empanadas.userRet["sum"] = sumUser;
+}
 
 
-})();
 
-//empanadas.init()
